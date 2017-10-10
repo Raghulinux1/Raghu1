7b726e25ef2bfe7eb93f3c93dd032102dc4b006a
Raghu <raghulinux1@gmail.com>
2017-10-10 09:24:56 +0000

    change

--- src/main/java/com/digi/proj1/App.java
+++ src/main/java/com/digi/proj1/App.java
@@ -6,7 +6,7 @@ package com.digi.proj1;
  */
 public class App 
 {
-    public static void main( String[] args )
+    public njstatic void main( String[] args )
     {
         System.out.println( "Devops!" );
     }
