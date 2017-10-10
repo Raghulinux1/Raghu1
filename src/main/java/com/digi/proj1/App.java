b215b8f41b60d673abe493d2adbddd60f0ff1495
Raghu <raghulinux1@gmail.com>
2017-10-10 09:55:36 +0000

    corrcted

--- src/main/java/com/digi/proj1/App.java
+++ src/main/java/com/digi/proj1/App.java
@@ -6,7 +6,7 @@ package com.digi.proj1;
  */
 public class App 
 {
-    public njstatic void main( String[] args )
+    public static void main( String[] args )
     {
         System.out.println( "changed!" );
     }
